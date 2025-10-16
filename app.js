4cfc04f29acad47011bec8a1c374483055ebf4e8
carlosAnao <joao.zibarth.santos@escola.pr.gov.br>
2025-10-16 07:30:42 -0300

    a

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 function criaCartao(categoria, pergunta, resposta) {
     let container = document.getElementById('cantainer');
     let cartao = document.createElement('article');
-    cartao.className + 'cartao'
+    cartao.className = 'cartao';
 }
 
+cartao.innerHTML =
+            <div class="cartao_conteudo">
+                    <h3>Programação</h3>
+                    <div class="cartao_conteudo_pergunta">
+                        <p>O que é html?</p>
+                    </div>
+                    <div class="cartao_conteudo_resposta">
+                        <p>O javaScript é uma linguagem de marcação</p>
+                    </div>
+                </div>
+                
